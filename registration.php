e74f3f76e7d99c8ced3d047d3ec42dfbb3538743
bappa <bappa2du@gmail.com>
2019-09-15 17:14:36 +0600

    remove dependency

--- registration.php
+++ registration.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Mazeapi Software.
+ *
+ * @package   Mazeapi_ForceLogin
+ * @author    Mazeapi
+ * @license   https://mazeapi.com/license.html
+ */
+
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Mazeapi_ForceLogin',
