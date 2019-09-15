38318dea5d9321472606ba2cd1d32e846a07c082
bappa <bappa2du@gmail.com>
2019-09-15 19:12:08 +0600

    update

---
+++ Model/Config/Source/RedirectAfterLogin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Mageapi\ForceLogin\Model\Config\Source;
+
+
+class RedirectAfterLogin
+{
+	public function toOptionArray()
+    {
+    	return [
+    		['value' => '/', 'label' => __('Homepage')],
+    		['value' => '/customer/account', 'label' => __('Customer Dashboard')],
+    	];	
+    }
+}
